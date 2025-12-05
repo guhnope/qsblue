396c74110226c9809d153f971892a6d7fc0b28ab
MGS <dane.butler@proton.me>
2025-12-05 18:29:56 +1000

    fix dracut

--- files/scripts/misc.sh
+++ files/scripts/misc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-dracut --force
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
